dedde81f04d1b9659c33a44fa8ec9f90878634ae
khanh <namkhanh18051995@gmail.com>
2021-04-13 17:20:38 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { WeatherComponent } from './modules/weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: MainLayoutComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
